246aa480f5e0db755921edc0d296fb9efba965f2
lisa <51735557+wanglixcg@users.noreply.github.com>
2019-07-30 09:31:19 +0800

    remove non-text

---
+++ wiki-remove.py
@@ -0,0 +1,31 @@
+import re
+
+print('主程序执行开始...')
+
+input_file_name = 'wiki_separate.txt'
+output_file_name = 'wiki_separate_remove.txt'
+input_file = open(input_file_name, 'r', encoding='utf-8')
+output_file = open(output_file_name, 'w', encoding='utf-8')
+
+print('开始读入数据文件...')
+lines = input_file.readlines()
+print('读入数据文件结束！')
+
+print('分词程序执行开始...')
+count = 1
+cn_reg = '^[\u4e00-\u9fa5]+$'
+
+for line in lines:
+    line_list = line.split('\n')[0].split(' ')
+    line_list_new = []
+    for word in line_list:
+        if re.search(cn_reg, word):
+            line_list_new.append(word)
+    print(line_list_new)
+    output_file.write(' '.join(line_list_new) + '\n')
+    count += 1
+    if count % 10000 == 0:
+        print('目前已分词%d条数据' % count)
+print('分词程序执行结束！')
+
+print('主程序执行结束！')
